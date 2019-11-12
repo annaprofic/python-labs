c567d3a839ef4c793a471bbf511ff450d1582ea9
sarnavskaa <sarnavskaa@gmail.com>
2019-11-12 16:07:38 +0100

    refactoring

--- lab6/times.py
+++ lab6/times.py
@@ -2,14 +2,11 @@ import unittest
 
 
 class Time:
-    """Klasa reprezentująca odcinek czasu."""
 
     def __init__(self, s=0):
-        """Zwraca instancję klasy Time."""
         self.s = int(s)
 
     def __str__(self):
-        """Zwraca string 'hh:mm:ss'."""
         h = self.s // 3600
         sec = self.s - h * 3600
         m = sec // 60
@@ -17,11 +14,9 @@ class Time:
         return "{0:02d}:{1:02d}:{2:02d}".format(h, m, sec)
 
     def __repr__(self):
-        """Zwraca string 'Time(s)'."""
         return "Time({0})".format(self.s)
 
     def __add__(self, other):
-        """Dodawanie odcinków czasu."""
         return Time(self.s + other.s)
 
     def __eq__(self, other):
@@ -42,8 +37,7 @@ class Time:
     def __ge__(self, other):
         return self.s >= other.s
 
-    def __int__(self):  # int(time1)
-        """Konwersja odcinka czasu do int."""
+    def __int__(self): 
         return self.s
 
 
