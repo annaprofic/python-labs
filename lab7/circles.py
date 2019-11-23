039e3f8180102b1649d946bde9820c32ef241b91
sarnavskaa <sarnavskaa@gmail.com>
2019-11-23 14:52:02 +0100

    completed task 7.5 circles.py

--- lab7/circles.py
+++ lab7/circles.py
@@ -1,6 +1,7 @@
 from lab6.points import Point
-import math
+from math import sqrt, pi
 import unittest
+
 
 class Circle:
 
@@ -11,7 +12,7 @@ class Circle:
         self.radius = radius
 
     def __repr__(self):   # "Circle(x, y, radius)"
-        return f"Circle({self.x}, {self.y}, {self.radius})"
+        return f"Circle({self.pt.x}, {self.pt.y}, {self.radius})"
 
     def __eq__(self, other):
         return self.pt == other.pt and self.radius == other.radius
@@ -20,24 +21,49 @@ class Circle:
         return not self == other
 
     def area(self):
-        return "%.4f" % (math.pi * self.radius ** 2)
+        return "%.4f" % (pi * self.radius ** 2)
 
     def move(self, x, y):
-        self.pt += Point(x, y)
+        self.pt = Point(self.pt.x + x, self.pt.y + y)
         return self
 
-    def cover(self, other): pass  # okrąg pokrywający oba
+    def cover(self, other):
+        covering_circle = self if self.radius > other.radius else other
+        center_x = (self.pt.x + other.pt.x) // 2
+        center_y = (self.pt.y + other.pt.y) // 2
+        equation = round(sqrt(pow(covering_circle.pt.x - center_x, 2) + pow(covering_circle.pt.y - center_y, 2)), 2)
 
+        covering_circle.pt = Point(center_x, center_y)
+        covering_circle.radius += equation
 
-# Kod testujący moduł.
+        return covering_circle
 
 
 class TestCircle(unittest.TestCase):
+
     def setUp(self):
         self.circle1 = Circle(1, 2, 5)
         self.circle2 = Circle(-1, 2, 5)
         self.circle3 = Circle(0, 4, 7)
         self.circle4 = Circle(3, -2, 8)
+
+    def test__repr__(self):
+        self.assertEqual("Circle(1, 2, 5)", repr(self.circle1))
+        self.assertEqual("Circle(-1, 2, 5)", repr(self.circle2))
+        self.assertEqual("Circle(0, 4, 7)", repr(self.circle3))
+        self.assertEqual("Circle(3, -2, 8)", repr(self.circle4))
+
+    def test__eq__(self):
+        self.assertTrue(self.circle1 == Circle(1, 2, 5))
+        self.assertTrue(self.circle2 == Circle(-1, 2, 5))
+        self.assertFalse(self.circle3 == Circle(1, 1, 1))
+        self.assertFalse(self.circle4 == Circle(3, 2, 8))
+
+    def test__ne__(self):
+        self.assertFalse(self.circle1 != Circle(1, 2, 5))
+        self.assertFalse(self.circle2 != Circle(-1, 2, 5))
+        self.assertTrue(self.circle3 != Circle(1, 1, 1))
+        self.assertTrue(self.circle4 != Circle(3, 2, 8))
 
     def test_area(self):
         self.assertEqual('78.5398', self.circle1.area())
@@ -46,10 +72,15 @@ class TestCircle(unittest.TestCase):
         self.assertEqual('201.0619', self.circle4.area())
 
     def test_move(self):
-        self.assertEqual(self.circle1, self.circle1.move(1, 2))
-        self.assertEqual('', self.circle1.move(0, 0))
-        self.assertEqual('', self.circle1.move(-1, -10))
-        self.assertEqual('', self.circle1.move(-1.5, 2.5))
+        self.assertEqual(Circle(2, 4, self.circle1.radius), self.circle1.move(1, 2))
+        self.assertEqual(self.circle2, self.circle2.move(0, 0))
+        self.assertEqual(Circle(-1, -6, self.circle3.radius), self.circle3.move(-1, -10))
+        self.assertEqual(Circle(1.5, 0.5, self.circle4.radius), self.circle4.move(-1.5, 2.5))
+
+    def test_cover(self):
+        self.assertEqual(Circle(0, 2, 6.0), self.circle1.cover(self.circle2))
+        self.assertEqual(Circle(0, 3, 8.0), self.circle2.cover(self.circle3))
+        self.assertEqual(Circle(1, 0, 10.83), self.circle3.cover(self.circle4))
 
     def tearDown(self):
         pass
