1b56c9bbe02a678567910d6abb85049f8386c548
sarnavskaa <sarnavskaa@gmail.com>
2019-12-01 19:44:24 +0100

    changed 8.1

--- lab8/linear_equation.py
+++ lab8/linear_equation.py
@@ -1,6 +1,3 @@
-from math import sqrt
-
-
 def check_args(a, b, c):
     if not isinstance(a, (int, float)):
         raise ValueError("Bad argument 'a'.")
@@ -11,27 +8,21 @@ def check_args(a, b, c):
     if not isinstance(c, (int, float)):
         raise ValueError("Bad arguments 'c'.")
 
-    if a == 0:
-        raise ValueError("Argument 'a' must be positive.")
+    if a == 0 and b == 0:
+        raise ValueError("Arguments 'a', 'b' must be positive.")
 
 
 def solve1(a, b, c):
     check_args(a, b, c)
 
-    delta = (b * b - 4 * a * c)
-
-    if delta < 0:
-        raise ValueError("Result doesn't exist.")
-
-    if delta == 0:
-        return "x1 = " + str(round(((-b) / (2 * a)), 2))
+    if a == 0 and b != 0:
+        return f"{-c / b}"
+    if a != 0 and b == 0:
+        return f"{-c / a}"
     else:
-        return "x1 = " + str(round(((-b + sqrt(delta)) / (2 * a)), 2)) +\
-               ", x2 = " + str(round(((-b - sqrt(delta)) / (2 * a)), 2))
+        return f" {- (a / b)} * x + {- (c / b)}"
 
 
-assert 'x1 = -5.0, x2 = 1.0' == str(solve1(-2, -8, 10))
-assert 'x1 = 4.0, x2 = -1.0' == str(solve1(1, -3, -4))
-assert 'x1 = 2.0, x2 = -2.0' == str(solve1(1, 0, -4))
-assert 'x1 = 7.0, x2 = 0.0' == str(solve1(1, -7, 0))
-assert 'x1 = -0.2, x2 = -1.0' == str(solve1(5, 6, 1))
+assert '9.0 * x + 2.0' == solve1(63, -7, 14)
+assert '22.0' == str(solve1(0, 0.5, -11))
+
