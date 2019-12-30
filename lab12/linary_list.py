1452d5bcd9af20a65cc6d1a22b16c89bbfe6799f
sarnavskaa <sarnavskaa@gmail.com>
2019-12-30 14:07:22 +0100

    completed task 12.1

---
+++ lab12/linary_list.py
@@ -0,0 +1,26 @@
+import random as rd
+
+
+def gen_random_list(k, n=100):
+    temp_l = []
+    for i in range(n):
+        temp_l.append(rd.randint(0, k - 1))
+    return temp_l
+
+
+def find_all_occurrences(k=10):
+
+    elements = gen_random_list(k)
+    seeked = rd.randint(0, k - 1)
+    occurrences = 0
+
+    for current in elements:
+        if current == seeked:
+            occurrences += 1
+
+    print(f"\ngenerated list: {elements}.")
+    print(f"\nsearching for '{seeked}' element...")
+    print(f"{occurrences} occurrences of '{seeked}' found.")
+
+
+find_all_occurrences()
