6a0cfceb30cd95937e6fd242509f8b8a70eaca99
sarnavskaa <sarnavskaa@gmail.com>
2019-12-30 14:23:09 +0100

    completed task 12.2

---
+++ lab12/binary_search.py
@@ -0,0 +1,27 @@
+from math import floor
+
+test_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+test_list3 = [1, 2, 3, 4]
+test_list4 = [1, 2]
+
+
+def binary_rec(arr, left, right, y):
+
+    if left > right:
+        return None
+
+    mid = floor((left + right)/2)
+
+    if arr[mid] == y:
+        return mid
+    if arr[mid] > y:
+        return binary_rec(arr, left, mid - 1, y)
+    return binary_rec(arr, mid + 1, right, y)
+
+
+assert binary_rec(test_list1, 0, len(test_list1) - 1, 6) == 5
+assert binary_rec(test_list1, 0, len(test_list1) - 1, 2) == 1
+assert binary_rec(test_list1, 0, len(test_list1) - 1, 15) == 14
+
+assert binary_rec(test_list3, 0, len(test_list3) - 1, 5) is None
+assert binary_rec(test_list4, 0, len(test_list4) - 1, 3) is None
