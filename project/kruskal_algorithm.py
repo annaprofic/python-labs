f1e40f61671aae9208fed9c8726742544f7bba41
sarnavskaa <sarnavskaa@gmail.com>
2020-01-26 17:16:05 +0100

    docstrings added

--- project/kruskal_algorithm.py
+++ project/kruskal_algorithm.py
@@ -2,11 +2,15 @@ import time
 from typing import List
 
 
-# class which is representing edge between vertices
 class Edges:
 
-    # initializing vertex a and vertex b which are joined with w - weight
     def __init__(self, a, b, w):
+        """
+        Initialization of params with are representing Edge of graph.
+        :param a: the first vertex
+        :param b: the second vertex
+        :param w: the weight of edge between them
+        """
         self.a = a
         self.b = b
         self.weight = w
@@ -18,21 +22,26 @@ class Edges:
         return f"{str(self.a)} - {str(self.b)} weight: {str(self.weight)}"
 
 
-# class contains necessary methods of algorithm
 class KruskalAlgorithm:
 
     def __init__(self, file_name='graph.txt'):
+        """
+        Method load sorted graph, extract number of vertices and edges and create array of parents.
+        :param file_name: the name of file with is contain graph
+        """
         self.graph: List[Edges] = []
-
-        # loading graph from file and extraction of vertices and edges number
         self.number_of_vertices, self.number_of_edges = self.load_graph(file_name)
         self.parents = self.load_dependency()
 
-    # load graph from graph.txt file by default and fill array of vertices
     def load_graph(self, file):
+        """
+        Method loads graph and creat List of Edges, sort it by weight and count vertices and edges.
+        :param file: file name to load graph from (graph.txt by default)
+        :return: the number of vertices, number of edges
+        """
         file = open(file, "rt")
         vertices = set()
-        # create graph data structure with vertex 1 (a) vertex 2 (b) and weight
+
         for line in file:
             a, b, weight = (line.split())
             self.graph.append(Edges(int(a), int(b), int(weight)))
@@ -42,24 +51,39 @@ class KruskalAlgorithm:
         self.graph.sort(key=lambda w: w.weight)
         return len(vertices), len(self.graph)
 
-    # method to fill array of parents with is representing dependency between vertices
     def load_dependency(self):
+        """
+        Method to fill array of parents with is representing dependency between vertices.
+        :return: List comprehension from 0 to number of vertices.
+        """
         return [vertex for vertex in range(self.number_of_vertices + 1)]
 
-    # method to find if vertices have the same parents
     def find_parents(self, x):
+        """
+        Method to find if vertices have the same parents
+        :param x: graph vertex
+        :return: the vertex that is linked input vertex
+        """
         if self.parents[x] == x:
             return x
         return self.find_parents(self.parents[x])
 
-    # join vertices if they haven't the same parents, in that way one of them is a parent of another one
     def join(self, x, y):
+        """
+        Method to join vertices if they haven't the same parents,
+        in that way one of them is a parent of another one.
+        :param x: the first vertex
+        :param y: the second vertex
+        """
         find_x, find_y = self.find_parents(x), self.find_parents(y)
         if find_x != find_y:
             self.parents[find_x] = find_y
 
-    # main method to execute algorithm
     def run(self):
+        """
+        Main method to execute algorithm.
+        :return: Minimum spanning tree for input graph
+        """
         min_spanning_tree: List[Edges] = []
 
         for edge in range(len(self.graph)):
@@ -70,7 +94,6 @@ class KruskalAlgorithm:
             # check if two vertices can create a cycle and if they can't - join them
             if algorithm.find_parents(a) != algorithm.find_parents(b):
                 algorithm.join(a, b)
-                # addition of edge to spanning tree
                 min_spanning_tree.append(Edges(a, b, weight))
 
         return min_spanning_tree
